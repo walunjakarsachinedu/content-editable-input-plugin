bf3ae306066339ab7f7c1f35eaf0ce84a77a001b
Sachin Walunjakar <walunjakarsachin@gmail.com>
2025-10-03 06:37:45 +0530

    implemented LCS DP to find shortest edit script

--- src/main.ts
+++ src/main.ts
@@ -51,13 +51,51 @@ Optimization to consider :-
     → often reduces input size a lot.
 */
 const diff = (prevStr: string, str: string): Change[] => {
-  const dp = {}; 
-  /** Idx of prevStr. */
-  let i=0;
-  /** Idx of str. */
-  let j=0;
+  const next: HelperArgs["next"] = {};
+  // run dp & fill next with next optimal step
+  helper({prevStr, str, r: 0, c: 0, dp: {[prevStr.length]: {[str.length]: 0}}, next});
 
-  return helper({prevStr, str, r: 0, c: 0, dp: {}});
+  let r=0, c=0;
+  const changes: Change[] = [];
+  let prevChange: Change|null = null;
+
+  while((r <= prevStr.length || c <= str.length) && next[r]?.[c]) {
+    const {r: nr, c: nc} = next[r][c];
+    const dr = nr - r;
+    const dc = nc - c;
+
+    if((dr > 0) != (dc > 0)) {
+      const cost = Math.max(dr, dc);
+      const op = (dr > 0) ? "delete" : "insert";
+      if(prevChange?.op == op && prevChange.pos + prevChange.textLength == r) {
+        prevChange.textLength += cost;
+      }
+      else {
+        const change: Change = {op, pos: r, textLength: cost};
+        changes.push(change);  
+        prevChange = change;
+      }
+    }
+
+    r = nr ?? prevStr.length;
+    c = nc ?? str.length;
+  }
+
+  // TODO: handle case where one string process and other pending
+  const itemsRemainToDelete = prevStr.length - (r + 1);
+  const itemsRemainToInsert = str.length - (c + 1);
+  if(itemsRemainToDelete > 0) {
+    changes.push({op: "delete", pos: r, textLength: itemsRemainToDelete})
+  }
+  if(itemsRemainToInsert > 0) {
+    changes.push({op: "insert", pos: r, textLength: itemsRemainToDelete})
+  }
+
+  if(r == prevStr.length - 1 && c == str.length - 1 && prevStr[r] != str[c]) {
+    changes.push({op: "delete", pos: r, textLength: 1})
+  }
+
+  return changes;
 };
 
 type HelperArgs = {
@@ -68,25 +106,52 @@ type HelperArgs = {
   /** Idx of str. */
   c: number, 
   /** Used for memoization and choosing optimal path. */
-  dp: Record<number, Record<number, number>>
+  dp: Record<number, Record<number, number>>,
+  /** Track next step optimal step taken. */
+  next: Record<number, Record<number, { r: number, c: number }>>
 };
 
-const helper = (args: HelperArgs): Change[] => {
-  const { prevStr, str, r, c, dp } = args;
-  /** represent prevStr.length */
-  let height = prevStr.length;
-  /** represent str.length */
-  let width = str.length;
+const helper = (args: HelperArgs): number => {
+  const { prevStr, str, r, c, dp, next } = args;
+  dp[r] ??= {};
+
+  if(r > prevStr.length || c > str.length) return Number.MAX_SAFE_INTEGER;
+  if(r == prevStr.length && c == str.length) return 0;
+  if(dp[r][c] != undefined) return dp[r][c];
+
+  next[r] ??= {};
+
+  if((r == prevStr.length) != (c == str.length)) {
+    if(r == prevStr.length) {
+      next[r][c] = { r, c: str.length };
+      const insert = str.length - (c + 1);
+      return dp[r][c] = insert;
+    } 
+    else {
+      next[r][c] = { r: prevStr.length, c };
+      const remove = prevStr.length - (r + 1);
+      return dp[r][c] = remove;
+    }
+  }
 
   if(prevStr[r] == str[c]) {
-    const diagnoal = helper({...args, r: r+1, c: c+1});
+    next[r][c] = {r: r+1, c: c+1};
+    return dp[r][c] = helper({...args, r: r+1, c: c+1, });
   }
-  const insert = helper({...args, r: r+1, c: c})
-  const remove = helper({...args, r: r, c: c+1})
+  const insert = helper({...args, r: r+1, c: c});
+  const remove = helper({...args, r: r, c: c+1});
 
-  return [];
+  next[r][c] = (insert < remove) 
+    ? {r: r+1, c: c} 
+    : next[r][c] = {r: r, c: c+1};
+  
+  return dp[r][c] = Math.min(insert, remove) + 1;
 } 
 
-console.log(diff("1slkj0,0000", "1,00,000"));
+// console.log(diff("1sdfj,0,00wes0", "10,000"));
+const prevStr = "33212"; // "221"
+const str = "221";
+console.log(`diff("${prevStr}", "${str}") →\n`);
+console.log(diff(prevStr, str))
 
 
