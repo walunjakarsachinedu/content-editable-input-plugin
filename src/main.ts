cd5082a10fe5692614f2b778f2070c30430a32a6
Sachin Walunjakar <walunjakarsachin@gmail.com>
2025-10-06 06:44:36 +0530

    computed new caret position from diff changes

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-import { diff as lcsdiff } from "./lcs_dp_diff";
+import { getCaretPos } from "./caret";
+import { diff } from "./diff/myers_diff";
 
-import { diff } from "./myers_diff";
 
-
-const prevStr = "1sdf1sd21";
-const str = "d1f1212";
+const prevStr = "1sdf12sd21";
+const str = "z34d1f1212";
 console.log(
   `diff(${prevStr}, ${str}) =\n`
-);
-
-console.log("diff using lcs dp algorithm →")
-console.log(
-  lcsdiff(prevStr, str)
 );
 
 console.log("diff using myer's diff algorithm →")
 console.log(
   diff(prevStr, str)
+);
+
+
+console.log(
+  `getCaretPos(${prevStr}, ${str}, 3)`, 
+  getCaretPos(prevStr, str, 3)
 );
 
 
