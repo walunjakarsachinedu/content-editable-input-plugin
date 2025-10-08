6070a3d5cd1d3f47e14fb6486f52757901b58dfd
Sachin Walunjakar <walunjakarsachin@gmail.com>
2025-10-08 06:42:39 +0530

    fixed changeBuilder

--- src/diff/changes_builder.ts
+++ src/diff/changes_builder.ts
@@ -1,29 +1,43 @@
 import { Change, Pos } from "./types";
 
 export const changeBuilder = (path: Pos[]): Change[] => {
-  let pr=0, pc=0;
   const changes: Change[] = [];
   let prevChange: Change|null = null;
 
   for(let i=1; i<path.length; i++) {
+    let {r:pr, c:pc}= path[i-1];
     const {r, c} = path[i];
     const dr = r - pr;
     const dc = c - pc;
     if((dr > 0) != (dc > 0)) {
-      const cost = Math.max(dr, dc);
-      const op = (dr > 0) ? "delete" : "insert";
-      if(prevChange?.op == op && prevChange.pos + prevChange.textLength == pr) {
-        prevChange.textLength += cost;
+      if(dr > 0) {
+        if(prevChange?.op == "delete") {
+          if(prevChange.pos + prevChange.textLength + 1 == r) {
+            prevChange.textLength += dr;
+          }
+        }
+        else {
+          const change: Change = {op: "delete", pos: pr, textLength: dr};
+          changes.push(change);  
+          prevChange = change;
+        }
       }
-      else {
-        const change: Change = {op, pos: pr, textLength: cost};
-        changes.push(change);  
-        prevChange = change;
+      if(dc > 0) {
+        if(prevChange?.op == "insert") {
+          if(prevChange.pos == r) {
+            prevChange.textLength += dc;
+          }
+        }
+        else {
+          const change: Change = {op: "insert", pos: pr, textLength: dc};
+          changes.push(change);  
+          prevChange = change;
+        }
       }
     }
-
-    pr = r;
-    pc = c;
+    else if(dr > 0 && dc > 0) {
+      prevChange = null;
+    }
   }
 
   return changes;
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
 import { getCaretPos } from "./caret";
-import { diff } from "./diff/myers_diff";
 
 
 const prevStr = "1sdf12sd21";
 const str = "z34d1f1212";
-console.log(
-  `diff(${prevStr}, ${str}) =\n`
-);
 
-console.log("diff using myer's diff algorithm →")
-console.log(
-  diff(prevStr, str)
-);
-
-
-console.log(
-  `getCaretPos(${prevStr}, ${str}, 3)`, 
-  getCaretPos(prevStr, str, 3)
-);
+console.log(getCaretPos(prevStr, str, 3));
 
 
